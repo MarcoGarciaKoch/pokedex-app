6ce6d043c45da017ecc46317662b9bb05fc82ecf
marcogarciakoch <marcogarciakoch@gmail.com>
2022-07-19 12:47:19 +0200

    routes-update

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import PokeList from './Pages/Home/Components/PokeList';
 import Contact from './Pages/Contact';
 import Details from './Pages/Details';
 import NotFound from './Pages/NotFound';
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <main className='app'>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PokeList />} />
+        <Route path="/pokedex-app" element={<PokeList />} />
         <Route path="/details/:id" element={<Details />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Navigate to="/pokedex-app" replace />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/SharedComponents/Nav/index.jsx
+++ src/SharedComponents/Nav/index.jsx
@@ -8,7 +8,7 @@ function Nav ({searchPokemon}) {
 
     return (
         <nav className='nav__container'>
-            <img onClick={() => navigate('/')} className='nav__logo-ball' src={require('../../assets/ultra-bola.png')} alt="Navigator logo" />
+            <img onClick={() => navigate('/pokedex-app')} className='nav__logo-ball' src={require('../../assets/ultra-bola.png')} alt="Navigator logo" />
             <section className='nav-search__container'>
                 <input className='pokemon-search__input' onChange={searchPokemon} type="text" name="search" id="SEARCH" placeholder='Find Your Pokemon'/>
                 <Link className='nav__subscription' to='/contact'>SUSCRIPTION</Link>
