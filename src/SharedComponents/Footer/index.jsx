555250412609ddc9e001df34b1a83095c383bfcf
marcogarciakoch <marcogarciakoch@gmail.com>
2022-07-24 12:25:26 +0200

    footer-and-sharing-social-media-style-update

--- src/SharedComponents/Footer/index.jsx
+++ src/SharedComponents/Footer/index.jsx
@@ -8,6 +8,10 @@ function Footer () {
     return (
         <div className='footer__container'>
             <h1>@ Marco Garcia Koch - 2022</h1>
+            <div className='github-info__container'>
+                <div className='github-logo'></div>
+                <h1>Check the GitHub repository <a className='github-link' rel="noopener noreferrer" target="_blank" href="https://github.com/MarcoGarciaKoch/pokedex-app">here</a></h1>
+            </div>
         </div>
     )
 }
